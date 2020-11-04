5c5b50d27e3508b19ce23a8bd6d9c6883c831d41
Harsha <harsha.sam23@gmail.com>
2020-11-04 16:55:59 +0530

    smart-brain api commit

---
+++ controllers/profile.js
@@ -0,0 +1,19 @@
+const handleProfileGet = (req, res, db) => {
+    const { id } = req.params;
+    db.select('*').from('users').where({ 'id': id })
+        .then(user => {
+            if (!user.length) {
+                res.status(400).json('No user found!')
+            }
+            else {
+                res.json(user[0]);
+            }
+        })
+        .catch(err => {
+            res.status(400).json("Error getting user")
+        })
+}
+
+module.exports = {
+    handleProfile: handleProfileGet
+}
---
+++ controllers/signin.js
@@ -0,0 +1,32 @@
+const handleSignin = (db, bcrypt) => (req, res) => {
+    const { email, pass } = req.body;
+    if (!email || !pass) {
+        return res.status(400).json("incorrect form submission");
+    }
+    db.select('email', 'hash').from('login').where({
+        email: email
+    })
+        .then(data => {
+            const isValid = bcrypt.compareSync(pass, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => {
+                        res.status(400).json("Unable to get user");
+                    })
+            }
+            else {
+                res.status(400).json("Wrong credentials");
+            }
+        })
+        .catch(err => {
+            res.status(400).json("Wrong credentials");
+        })
+}
+
+module.exports = {
+    handleSignin: handleSignin
+}
