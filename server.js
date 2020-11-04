24d27bd690e676302cfe9f01c195272e60489366
Harsha <harsha.sam23@gmail.com>
2020-11-04 19:26:03 +0530

    added response for / get request

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const app = express();
 app.use(cors());
 app.use(bodyparser.json());
 
+app.get('/', (req, res) => { res.json('It is working') });
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
